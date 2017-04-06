6b485e13bab8b051482d91df087927fbb0d8342b
Dmitry Levashov <dio@std42.ru>
2017-04-06 12:55:32 +0300

    tests done

--- tests/integration/components/heatmap-layer-test.js
+++ tests/integration/components/heatmap-layer-test.js
@@ -1,25 +1,66 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
+import HeatmapLayer from 'ember-leaflet-heatmap-layer/components/heatmap-layer';
 import hbs from 'htmlbars-inline-precompile';
 
+const { run } = Ember;
+
+var layer;
+
 moduleForComponent('heatmap-layer', 'Integration | Component | heatmap layer', {
-  integration: true
+  integration: true,
+
+  beforeEach() {
+    this.register('component:heatmap-layer', HeatmapLayer.extend({
+      init() {
+        this._super(...arguments);
+        layer = this;
+      }
+    }));
+  }
 });
 
 test('it renders', function(assert) {
+  const done = assert.async()
+  this.set('data', [{lat:24.6408,lng:46.7728,count:3},{lat:50.75,lng:-1.55,count:1},{lat:52.6333,lng:1.75,count:1},{lat:48.15,lng:9.4667,count:1}]);
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  const props = {
+    minValue: 1,
+    maxValue: 5,
+    valueField: 'count',
+    minOpacity: 0.1,
+    maxOpacity: 0.9,
+    radius: 4
+  };
 
-  this.render(hbs`{{heatmap-layer}}`);
-
-  assert.equal(this.$().text().trim(), '');
+  this.setProperties(props);
 
   // Template block usage:
   this.render(hbs`
-    {{#heatmap-layer}}
-      template block text
-    {{/heatmap-layer}}
+    {{#leaflet-map lat=55.753445 lng=37.620418 zoom=10}}
+      {{heatmap-layer
+        data=data
+        minValue=minValue
+        maxValue=maxValue
+        valueField=valueField
+        minOpacity=minOpacity
+        maxOpacity=maxOpacity
+        radius=radius
+      }}
+    {{/leaflet-map}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  run.next(() => {
+    const leafletHeatmap = layer._layer;
+    assert.equal(leafletHeatmap._data.length, 4);
+
+    for (var i in props) {
+      if (props.hasOwnProperty(i)) {
+        assert.equal(leafletHeatmap.cfg[i], props[i], `${i} equal ${props[i]}`);
+      }
+    }
+    done();
+  });
+
+
 });
